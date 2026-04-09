36d6c5bc55d7d849c1cdc993534211e01aaccbbc
kstrodeveloper <aynerjosec@gmail.com>
2026-04-08 23:10:00 -0500

    feat: implementar metodos guardar y eliminar mediante Spring + JPA.

--- ZonaFitSpringBoot/src/main/java/gm/zona_fit/service/ClienteServicio.java
+++ ZonaFitSpringBoot/src/main/java/gm/zona_fit/service/ClienteServicio.java
@@ -14,11 +14,11 @@ import org.springframework.stereotype.Service;
 public class ClienteServicio implements IClienteServicio {
 
     @Autowired
-    private ClienteRepositorio clienteRepositorio; 
+    private ClienteRepositorio clienteRepositorio;
 
     /*
-    *Usamos findAll() porque regresa todos los objetos de tipo cliente
-    * en la tabla de clientes.
+     * Usamos (findAll()) porque regresa todos los objetos de tipo cliente
+     * en la tabla de clientes.
      */
     @Override
     public List<Cliente> listarClientes() {
@@ -26,21 +26,30 @@ public class ClienteServicio implements IClienteServicio {
         return clientes;
     }
 
-/*
-* * Usamos findById porque regresa un valor optional.
-* * Luego usamos orElse porque si no hay registros BD retorna null. 
- */
+    /*
+     * Usamos (findById()) porque regresa un valor optional.
+     * Luego usamos (orElse()) porque si no hay registros BD retorna null.
+     */
     @Override
     public Cliente buscarClientePorId(Integer idCliente) {
         Cliente cliente = clienteRepositorio.findById(idCliente).orElse(null);
         return cliente;
     }
 
+    /*
+     * Usamos (save()) porque guarda el objeto de tipo cliente y internamente
+     * si ID = NULL => INSERT o UPDATE.
+     */
+
     public void guardarCliente(Cliente cliente) {
-        return;
+        clienteRepositorio.save(cliente);
     }
 
+    /*
+     * Usamos (delete()) porque toma el id de forma automatica y
+     * si encuentra el id en la BD lo elimina.
+     */
     public void eliminarCLiente(Cliente cliente) {
-        return;
+        clienteRepositorio.delete(cliente);
     }
 }
