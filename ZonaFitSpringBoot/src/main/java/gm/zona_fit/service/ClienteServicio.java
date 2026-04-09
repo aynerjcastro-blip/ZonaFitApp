fc6d68078c7dac41117365a7b947fb9f9ab14fc1
kstrodeveloper <aynerjosec@gmail.com>
2026-04-08 23:01:56 -0500

    feat: implemenatacion de listar y bucarPorId mediante Spring + JPA.

--- ZonaFitSpringBoot/src/main/java/gm/zona_fit/service/ClienteServicio.java
+++ ZonaFitSpringBoot/src/main/java/gm/zona_fit/service/ClienteServicio.java
@@ -2,6 +2,9 @@ package gm.zona_fit.service;
 
 import java.util.List;
 import gm.zona_fit.model.Cliente;
+import gm.zona_fit.repository.ClienteRepositorio;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,16 +13,27 @@ import org.springframework.stereotype.Service;
  */
 public class ClienteServicio implements IClienteServicio {
 
+    @Autowired
+    private ClienteRepositorio clienteRepositorio; 
+
+    /*
+    *Usamos findAll() porque regresa todos los objetos de tipo cliente
+    * en la tabla de clientes.
+     */
     @Override
     public List<Cliente> listarClientes() {
-        return null;
-
+        List<Cliente> clientes = clienteRepositorio.findAll();
+        return clientes;
     }
 
+/*
+* * Usamos findById porque regresa un valor optional.
+* * Luego usamos orElse porque si no hay registros BD retorna null. 
+ */
     @Override
-    public Cliente buscarClientePorId(Cliente cliente) {
-        return null;
-
+    public Cliente buscarClientePorId(Integer idCliente) {
+        Cliente cliente = clienteRepositorio.findById(idCliente).orElse(null);
+        return cliente;
     }
 
     public void guardarCliente(Cliente cliente) {
