b0fe8e7cc8767b2f45e3445db15ed4c011dc8fd1
kstrodeveloper <aynerjosec@gmail.com>
2026-04-08 22:50:27 -0500

    feat: operaciones CRUD con spring.

--- ZonaFitSpringBoot/src/main/java/gm/zona_fit/service/ClienteServicio.java
+++ ZonaFitSpringBoot/src/main/java/gm/zona_fit/service/ClienteServicio.java
@@ -1,11 +1,32 @@
 package gm.zona_fit.service;
 
+import java.util.List;
+import gm.zona_fit.model.Cliente;
 import org.springframework.stereotype.Service;
 
 @Service
 /*
-* *Notacion Spring (@Service).
-*/
-public class ClienteServicio {
-    
+ * *Notacion Spring (@Service).
+ */
+public class ClienteServicio implements IClienteServicio {
+
+    @Override
+    public List<Cliente> listarClientes() {
+        return null;
+
+    }
+
+    @Override
+    public Cliente buscarClientePorId(Cliente cliente) {
+        return null;
+
+    }
+
+    public void guardarCliente(Cliente cliente) {
+        return;
+    }
+
+    public void eliminarCLiente(Cliente cliente) {
+        return;
+    }
 }
