853523ccc26427e8a1174fd99de155bc55c532f4
kstrodeveloper <aynerjosec@gmail.com>
2026-04-10 00:11:31 -0500

    feat: levantar objetos mediante fabirca de spring.

--- ZonaFitSpringBoot/src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ ZonaFitSpringBoot/src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -5,11 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import ch.qos.logback.classic.Logger;
-
-import org.slf4j.LoggerFactory;
-
 import gm.zona_fit.service.IClienteServicio;
 
 @SpringBootApplication
@@ -27,8 +23,10 @@ public class ZonaFitApplication {
 	private static final Logger logger = (Logger) LoggerFactory.getLogger(ZonaFitApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Iniciando App...");
+		// Levantamos fabrica de Spring
 		SpringApplication.run(ZonaFitApplication.class, args);
-
+		logger.info("Aplicacion Finalizada");
 	}
 
 }
