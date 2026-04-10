d160b4ad8880a5c63ebef5aca6576d378bc1a373
kstrodeveloper <aynerjosec@gmail.com>
2026-04-10 00:01:00 -0500

    feat: inyectar dependencias service en presentation.

--- ZonaFitSpringBoot/src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ ZonaFitSpringBoot/src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -1,13 +1,25 @@
 package gm.zona_fit;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import gm.zona_fit.service.IClienteServicio;
+
 @SpringBootApplication
 public class ZonaFitApplication {
+	/**
+	 **Implementamos la interface porque asi inyectamos una dependencia de services a nuestra capa de presentacion.
+	 */
+	@Autowired
+	private IClienteServicio clienteServicio;
+
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZonaFitApplication.class, args);
+
 	}
 
 }
--- ZonaFitSpringBoot/src/main/java/gm/zona_fit/service/IClienteServicio.java
+++ ZonaFitSpringBoot/src/main/java/gm/zona_fit/service/IClienteServicio.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface IClienteServicio {
     public List<Cliente> listarClientes();
 
-    public Cliente buscarClientePorId (Cliente cliente);
+    public Cliente buscarClientePorId (Integer idCliente);
     public void guardarCliente(Cliente cliente);
     public void eliminarCLiente(Cliente cliente);
     
