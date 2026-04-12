bf43f80d8a1e37d5fd0e784b8a3d6e9e1b37ae54
kstrodeveloper <aynerjosec@gmail.com>
2026-04-11 23:31:28 -0500

    feat(spring): agregar menu de operaciones CRUD.

--- ZonaFitSpringBoot/src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ ZonaFitSpringBoot/src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -3,13 +3,14 @@ package gm.zona_fit;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ch.qos.logback.classic.Logger;
 import gm.zona_fit.service.IClienteServicio;
 
 @SpringBootApplication
-public class ZonaFitApplication {
+public class ZonaFitApplication  implements CommandLineRunner{
 	/**
 	 **Implementamos la interface porque asi inyectamos una dependencia de services a nuestra capa de presentacion.
 	 */
@@ -24,9 +25,19 @@ public class ZonaFitApplication {
 
 	public static void main(String[] args) {
 		logger.info("Iniciando App...");
-		// Levantamos fabrica de Spring
+		// 1. Levantamos fabrica de Spring
 		SpringApplication.run(ZonaFitApplication.class, args);
 		logger.info("Aplicacion Finalizada");
 	}
-
+	//2. Luego ejecutamos el metodo run (master)
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("*** Aplicacion ZonaFit (Gym) ***");
+		logger.info("1. Agregar clientes.");
+		logger.info("2. Listar todos los clientes.");
+		logger.info("3. Modificar clientes");
+		logger.info("4. Buscar Cliente.");
+		logger.info("5. Eliminar cliente.");
+		logger.info("6. Salir");
+	}
 }
