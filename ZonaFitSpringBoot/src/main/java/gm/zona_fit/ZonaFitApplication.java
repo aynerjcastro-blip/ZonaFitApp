19f8e88541011d68512e9842854059f54e562236
kstrodeveloper <aynerjosec@gmail.com>
2026-04-11 23:57:21 -0500

    fix: correcion en ZonaFitApp y el logback.xml.

--- ZonaFitSpringBoot/src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ ZonaFitSpringBoot/src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -1,12 +1,14 @@
 package gm.zona_fit;
 
-
+import java.util.List;
+import java.util.Scanner;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ch.qos.logback.classic.Logger;
+import gm.zona_fit.model.Cliente;
 import gm.zona_fit.service.IClienteServicio;
 
 @SpringBootApplication
@@ -22,7 +24,10 @@ public class ZonaFitApplication  implements CommandLineRunner{
 	* Usamos la clase Logger (LoggerFactory) porque nos ayuda a solicitar el objeto de tipo Logger, indicando el .class. 
 	 */
 	private static final Logger logger = (Logger) LoggerFactory.getLogger(ZonaFitApplication.class);
-
+	/*
+	 *	Usamos lineSeparator() para imprimir un salto de linea de manera generica.	
+	 */
+	String nl = System.lineSeparator();
 	public static void main(String[] args) {
 		logger.info("Iniciando App...");
 		// 1. Levantamos fabrica de Spring
@@ -32,12 +37,32 @@ public class ZonaFitApplication  implements CommandLineRunner{
 	//2. Luego ejecutamos el metodo run (master)
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("*** Aplicacion ZonaFit (Gym) ***");
-		logger.info("1. Agregar clientes.");
-		logger.info("2. Listar todos los clientes.");
-		logger.info("3. Modificar clientes");
-		logger.info("4. Buscar Cliente.");
-		logger.info("5. Eliminar cliente.");
-		logger.info("6. Salir");
+		
+		zonaFitApp();
+	}
+	private void zonaFitApp(){
+		var salir = false;
+		var consola = new Scanner(System.in);
+		do {
+			var opcion = mostrarMenu(consola);
+			salir = ejecutarOpciones(consola,opcion);
+			logger.info(nl);
+		} while (!salir);
+	}
+	private int mostrarMenu(Scanner sc){
+		logger.info("""
+			\n*** Aplicacion ZonaFit (Gym) ***
+				1. Listar clientes.
+				2. Buscar cliente.
+				3. Agregar cliente.
+				4. Modificar cliente.
+				5. Eliminar cliente.
+				T¿Elige una opcion:
+				\s""");
+				return Integer.parseInt(sc.nextLine());
+	}	
+
+	private boolean ejecutarOpciones(Scanner consola, int opcion){
+		return false;
 	}
 }
